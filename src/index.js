d5d4eb7de3e042e0b9868b3c0e9275e828ca4377
Guilherme Reis Araújo <guih2127@gmail.com>
2022-05-03 15:12:35 -0300

    schema file and first mutation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 const { ApolloServer } = require("apollo-server");
 
-// 1 the variable typeDefs defines our graphQL schema, defining a simple Query
-// type with a field called info, a required string. It can't be null because of the !
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
-
-// 2 the variable resolvers is the actual implementation of our schema
 let links = [
   {
     id: "link-0",
@@ -27,21 +11,28 @@ let links = [
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
     feed: () => links,
   },
-  // 3
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
-// 3 the schema and resolvers are passed to ApolloServer to tell the server
-// what API operations are accepted
+const fs = require("fs");
+const path = require("path");
+
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
