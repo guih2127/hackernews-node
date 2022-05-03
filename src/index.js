9c962ff5809ecd7fcd8a03097a3a544f63b32b57
Guilherme Reis Araújo <guih2127@gmail.com>
2022-05-03 14:53:49 -0300

    new query and resolvers

--- src/index.js
+++ src/index.js
@@ -3,15 +3,38 @@ const { ApolloServer } = require("apollo-server");
 // 1 the variable typeDefs defines our graphQL schema, defining a simple Query
 // type with a field called info, a required string. It can't be null because of the !
 const typeDefs = `
-  type Query {
-    info: String!
-  }
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
 `;
 
 // 2 the variable resolvers is the actual implementation of our schema
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
