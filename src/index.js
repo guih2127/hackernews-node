c3b2c1f3d6f4e79f499273d6e9a073d80fcb1bfb
Guilherme Reis Araújo <guih2127@gmail.com>
2022-05-04 13:21:36 -0300

    authentication

--- src/index.js
+++ src/index.js
@@ -2,26 +2,18 @@ const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
+const { getUserId } = require("./utils");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (parents, args, context) => {
-      return context.prisma.link.findMany();
-    },
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-
-      return newLink;
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 const fs = require("fs");
@@ -30,8 +22,12 @@ const path = require("path");
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
